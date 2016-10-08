ba3be05b041dacf66a5bd427af09d7f346a6a5af
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2016-10-08 15:12:43 +0200

    changed some imports to decrease dependencies and make the code python 3 compatible

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='NatNetClient',
-      version='0.7',
+      version='0.8',
       description='Client for getting Optitrack data from Motive through the NatNet Stream.',
       author='Nicholas A. Del Grosso',
       author_email='delgrosso@bio.lmu.de',
